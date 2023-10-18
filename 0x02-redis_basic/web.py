dd37de3955e5c886474403f9ace41f5c2c3a7389
Wakjira Bekele <73541693+wakjirabekele@users.noreply.github.com>
2023-10-18 16:13:16 +0300

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,37 +1,37 @@
 #!/usr/bin/env python3
-"""Implementing an expiring web cache and tracker"""
-
-from functools import wraps
+"""web module
+"""
 import redis
 import requests
 from typing import Callable
+from functools import wraps
 
 r = redis.Redis()
 
 
 def count_requests(method: Callable) -> Callable:
-    """ Decortator for counting how many times a request
-    has been made """
-
+    """
+    count requests decorate
+    """
     @wraps(method)
     def wrapper(url):
-        """ Wrapper for decorator functionality """
+        """
+        Wrapper for decorator functionality
+        """
         r.incr(f"count:{url}")
-        cached_html = r.get(f"cached:{url}")
-        if cached_html:
-            return cached_html.decode('utf-8')
-
+        cache_html = r.get(f"cached:{url}")
+        if cache_html:
+            return cache_html.decode('utf-8')
         html = method(url)
         r.setex(f"cached:{url}", 10, html)
         return html
-
     return wrapper
 
 
 @count_requests
 def get_page(url: str) -> str:
-    """Uses the requests module to obtain the HTML
-    content of a particular URL and returns it.
     """
-    req = requests.get(url)
-    return req.text
+    get page method
+    """
+    request = requests.get(url)
+    return request.text
